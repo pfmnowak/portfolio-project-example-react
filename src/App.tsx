037b1c6275bebb38aa0a34154507f8570e1c2eb7
pfmnowak <58272232+pfmnowak@users.noreply.github.com>
2023-04-19 10:42:48 +0200

    Install react router, create routes

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,18 @@
+import { Navigate, Route, Routes } from 'react-router-dom';
 import './App.css';
 import Footer from './components/Footer';
+import HomePage from './pages/HomePage';
 
 function App() {
 	return (
 		<div className='App'>
 			<header className='App-header' />
+			<Routes>
+				<Route path='/' element={<HomePage />} />
+				{/* <Route path='/experience' element={<ExperiencePage />} /> */}
+				{/* <Route path='/services' element={<ServicesPage />} /> */}
+				<Route path='*' element={<Navigate to='/' replace />} />
+			</Routes>
 			<Footer />
 		</div>
 	);
---
+++ src/pages/HomePage.tsx
@@ -0,0 +1,11 @@
+import classes from './HomePage.module.scss';
+
+const HomePage = () => {
+	return (
+		<div className={classes.home}>
+			<h2>HOME PAGE</h2>
+		</div>
+	);
+};
+
+export default HomePage;
