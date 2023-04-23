1fe3a0cffa0190d1e389790c0c4ced64f741bca1
pfmnowak <58272232+pfmnowak@users.noreply.github.com>
2023-04-23 11:08:03 +0200

    Style the Header

--- src/components/HeaderSection/HeaderSection.tsx
+++ src/components/HeaderSection/HeaderSection.tsx
@@ -5,6 +5,27 @@ const HeaderSection = () => {
 	return (
 		<div className={classes['header-section']}>
 			<Header />
+
+			<div className={classes.content}>
+				<div className={classes['header-text']}>
+					<h1 className={`${classes['heading-primary']} ${classes['heading-primary--sub']}`}>
+						Hello!
+					</h1>
+					<h1 className={`${classes['heading-primary']} ${classes['heading-primary--main']}`}>
+						I am <span className={classes['heading-primary--color']}>HRS</span> Pathan Creative
+						UI/UX Designer
+					</h1>
+					<ol className={classes['skill-list']}>
+						<li>UI/UX Design</li>
+						<li>Product Design</li>
+						<li>Web Design</li>
+						<li>Front-end Design</li>
+					</ol>
+				</div>
+				<div className={classes['header-image']}>
+					<img src={require('../../img/model.png')} className={classes['model']} alt='model' />
+				</div>
+			</div>
 		</div>
 	);
 };
